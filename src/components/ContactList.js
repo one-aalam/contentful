766aa778909300f54a0d816b12a0b0d3ad028d4d
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-01-28 16:11:21 +0530

    Add more users to the team

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -12,6 +12,60 @@ const contacts = [
     name: 'Aftab Alam',
     avatar: 'https://pbs.twimg.com/profile_images/378800000663452972/02010f03a613c42cddca2ff42eadf48f_400x400.png',
     desg: 'CCO - Chief Creativity Officer'
+  },
+  {
+    id: 2,
+    name: 'Miyah Myles',
+    avatar: 'https://randomuser.me/api/portraits/women/44.jpg',
+    desg: 'CCO - Chief Creativity Officer'
+  },
+  {
+    id: 3,
+    name: 'Loren Spears',
+    avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+    desg: 'CCO - Chief Creativity Officer'
+  },
+  {
+    id: 4,
+    name: 'Cielo Luna',
+    avatar: 'https://pbs.twimg.com/profile_images/943467217303224322/3_5Qg0s9.jpg',
+    desg: 'CCO - Chief Creativity Officer'
+  },
+  {
+    id: 5,
+    name: 'koray okumus',
+    avatar: 'https://pbs.twimg.com/profile_images/1061275018926194690/6MWH9Xi_.jpg',
+    desg: 'CCO - Chief Creativity Officer'
+  },
+  {
+    id: 6,
+    name: 'Della Case',
+    avatar: 'https://randomuser.me/api/portraits/men/46.jpg',
+    desg: 'CCO - Chief Creativity Officer'
+  },
+  {
+    id: 7,
+    name: 'Aidyn Cody',
+    avatar: 'https://images-na.ssl-images-amazon.com/images/M/MV5BODFjZTkwMjItYzRhMS00OWYxLWI3YTUtNWIzOWQ4Yjg4NGZiXkEyXkFqcGdeQXVyMTQ0ODAxNzE@._V1_UX172_CR0,0,172,256_AL_.jpg',
+    desg: 'CCO - Chief Creativity Officer'
+  },
+  {
+    id: 8,
+    name: 'Harold Adams',
+    avatar: 'https://pbs.twimg.com/profile_images/969073897189523456/rSuiu_Hr.jpg',
+    desg: 'CCO - Chief Creativity Officer'
+  },
+  {
+    id: 9,
+    name: 'Yousif Cross',
+    avatar: 'https://randomuser.me/api/portraits/men/35.jpg ',
+    desg: 'CCO - Chief Creativity Officer'
+  },
+  {
+    id: 10,
+    name: 'Derrick Wells',
+    avatar: 'https://pbs.twimg.com/profile_images/1157046181698011136/xZ4wg2Xj.jpg',
+    desg: 'CCO - Chief Creativity Officer'
   }
 ]
 
--- src/pages/team.js
+++ src/pages/team.js
@@ -3,7 +3,7 @@ import ContactList from '../components/ContactList';
 
 export default () => (
   <Layout siteTitle={'Analytics Times -- Team'} description={'Yur at the Team of Contentful'}>
-    <div>Team</div>
+    {/* <h6 class=" dim black b f6 f-headline-ns tc db mb3 mb4-ns w-100">Team</h6> */}
     <ContactList/>
   </Layout>
 )
